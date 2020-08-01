752b9d2467eb0e9ccc69712aab80bdbe61c01fa9
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-08-02 00:24:28 +0200

    Improve tests
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- test/go/mod/cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/sagikazarmark/mypleasings/test/go/mod/lib"
-
-func main() {
-	println("Hello,", lib.World+"!")
-}
--- test/go/mod/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/sagikazarmark/mypleasings/test/go/mod
-
-go 1.15
